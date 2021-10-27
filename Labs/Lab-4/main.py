626c0be1b4463aaea97fdc350065490dfcd4dc46
Calandrinon <georgesut@yahoo.com>
2021-10-27 09:58:42 +0300

    Store both the key and the chain position from the symbol table

--- Labs/Lab-4/main.py
+++ Labs/Lab-4/main.py
@@ -1,6 +1,8 @@
 from scanner import Scanner
 
-scanner = Scanner("p3.txt")
+scanner = Scanner("p2.txt")
 symbol_table, program_internal_form = scanner.scan()
 print("Symbol table: {}".format(symbol_table))
 print("PIF: {}".format(program_internal_form))
+
+
--- Labs/Lab-4/scanner.py
+++ Labs/Lab-4/scanner.py
@@ -7,7 +7,7 @@ class Scanner:
     def __init__(self, input_filename):
         self.__separators = [' ', ';', ':', '{', '}', '[', ']', '(', ')', ',']
         self.__separators_for_regex_pattern = [' ', ';', ':', '\{', '\}', '\[', '\]', '\(', '\)']
-        self.__operators = ['+', '-', '*', '/', '%', '=', '==', '<', '>', '<=', '>=', '+=']
+        self.__operators = ['+', '-', '*', '/', '%', '=', '==', '<', '>', '<=', '>=', '+=', '**']
         self.__keywords = ["defvar", "deflist", "and", "or", "not", "if", "else", "loop", "input", "print", "int", "str", "char", "bool", "double"]
         self.__input_filename = input_filename
         self.__file = None
@@ -55,55 +55,53 @@ class Scanner:
         for separator in self.__separators_for_regex_pattern:
             separators_as_string += separator
         tokenization_pattern = '.*?[' + separators_as_string + ']'
-        self.__space_tokenized_code = list(map(lambda line: re.findall(tokenization_pattern, line), self.__file))
+        for index in range(0, len(self.__file)):
+            self.__file[index] = (self.__file[index], index)
+        self.__space_tokenized_code = list(map(lambda line: (list(map(lambda token: (token, line[1]), re.findall(tokenization_pattern, line[0])))), self.__file))
         print("Space tokenized code: {}".format(self.__space_tokenized_code))
-        self.__space_cleared_code = list(map(lambda row: list(map(lambda token: token.replace(' ', ''), row)), self.__space_tokenized_code))
+        self.__space_cleared_code = list(map(lambda row: list(map(lambda token: (token[0].replace(' ', ''), token[1]), row)), self.__space_tokenized_code))
         self.__space_cleared_code = [item for sublist in self.__space_cleared_code for item in sublist]
         print("Space cleared code: {}".format(self.__space_cleared_code))
-        self.__separator_tokenized_code = list(map(lambda token: self.extract_token_with_separators(token), self.__space_cleared_code))
+        self.__separator_tokenized_code = list(map(lambda token: (self.extract_token_with_separators(token[0]), token[1]), self.__space_cleared_code))
+        print("Separator tokenized code: {}".format(self.__separator_tokenized_code))
 
         final_result = []
         line_number = 0
 
         for item in self.__separator_tokenized_code:
-            if isinstance(item, str):
-                if len(item) > 1 and item[0] == '-':
-                    final_result.append('-')
-                    final_result.append(item[1:])
+            if isinstance(item[0], str):
+                if len(item[0]) > 1 and item[0][0] == '-':
+                    final_result.append(('-', item[1]))
+                    final_result.append((item[0][1:], item[1]))
                 else:
-                    final_result.append(item)
-
-                if item == '\n':
-                    line_number += 1
-                    print(item, line_number)
+                    final_result.append((item[0], item[1]))
             else:
-                final_result += item
+                final_result += list(map(lambda x: (x, item[1]), item[0]))
 
-        return list(filter(lambda x: x != '', final_result))
+        return list(filter(lambda x: x[0] != '', final_result))
 
     def scan(self):
         file_content = self.read_file()
         print(file_content)
         tokens = self.split_in_tokens()
-        print(tokens)
+        print("Tokenized code: {}".format(tokens))
         for token in tokens:
-            #print("Token {}".format(token))
-            if self.is_separator(token):
+            if self.is_separator(token[0]):
                 self.__program_internal_form.append((token, -1, 0))
-            elif self.is_operator(token):
+            elif self.is_operator(token[0]):
                 self.__program_internal_form.append((token, -1, 1))
-            elif self.is_keyword(token):
+            elif self.is_keyword(token[0]):
                 self.__program_internal_form.append((token, -1, 2))
-            elif self.is_identifier(token):
-                self.__symbol_table.add_element(token)
-                position_in_symbol_table = self.__symbol_table.get_element_position(token)
+            elif self.is_identifier(token[0]):
+                self.__symbol_table.add_element(token[0])
+                position_in_symbol_table = self.__symbol_table.search_element(token[0])
                 self.__program_internal_form.append((token, position_in_symbol_table, 3))
-            elif self.is_constant(token):
-                self.__symbol_table.add_element(token)
-                position_in_symbol_table = self.__symbol_table.get_element_position(token)
+            elif self.is_constant(token[0]):
+                self.__symbol_table.add_element(token[0])
+                position_in_symbol_table = self.__symbol_table.search_element(token[0])
                 self.__program_internal_form.append((token, position_in_symbol_table, 4))
             else:
-                print("Lexical error:{}".format(token))
+                print("Lexical error -> Token: {}; Line: {};".format(token[0], token[1]+1))
                 return (-1, -1)
 
         return (self.__symbol_table, self.__program_internal_form)
