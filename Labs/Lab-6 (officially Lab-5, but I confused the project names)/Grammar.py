028a58406d3ed4f409657ec7bc3b1841ca64e04a
Calandrinon <georgesut@yahoo.com>
2021-11-22 19:22:11 +0200

    Add the Grammar class
    g1.txt is an example file for a simple context-free grammar and g2.txt is the grammar of the minilanguage.

---
+++ Labs/Lab-6 (officially Lab-5, but I confused the project names)/Grammar.py
@@ -0,0 +1,56 @@
+class Grammar:
+    def __init__(self, filename):
+        self.__filename = filename
+        self.__filelines_with_productions = None
+        self.read_grammar()
+        self.__nonterminals = self.get_set_of_nonterminals()
+        self.__terminals = self.get_set_of_terminals()
+        self.__productions = self.__analyze_file_for_productions()
+
+    def read_grammar(self):
+        file_handler = open(self.__filename, "r")
+        self.__filelines_with_productions = list(map(lambda line: line.strip(), file_handler.readlines()))
+        self.__nonterminals = self.__filelines_with_productions[0].split(" ")
+        self.__terminals = self.__filelines_with_productions[1].split(" ")
+        self.__filelines_with_productions = list(map(lambda production: production.split("="), self.__filelines_with_productions[2:]))
+        return self.__filelines_with_productions
+
+    def remove_empty_character_from_list(self, input_list):
+        return list(filter(lambda x: x != '', input_list))
+
+    def get_set_of_nonterminals(self):
+        return self.__nonterminals
+
+    def get_set_of_terminals(self):
+        return self.__terminals
+
+    def get_set_of_productions(self):
+        return self.__productions
+
+    def __analyze_file_for_productions(self):
+        productions = {}
+
+        for production in self.__filelines_with_productions:
+            production[0] = production[0].replace(" ", "")
+            production[1] = list(map(lambda x: self.remove_empty_character_from_list(x.split(" ")), production[1].strip().split("|")))
+            productions[production[0]] = production[1]
+
+        return productions
+
+    def get_productions_of_a_nonterminal(self, nonterminal):
+        if nonterminal not in self.__nonterminals:
+            return None
+        return self.__productions[nonterminal]
+
+    def check_if_context_free(self):
+        file_handler = open(self.__filename, "r")
+        file_productions = list(map(lambda line: line.strip(), file_handler.readlines()[2:]))
+
+        for file_production in file_productions:
+            left_hand_side = file_production.split("=")[0]
+            left_hand_side_tokens = self.remove_empty_character_from_list(left_hand_side.split(" "))
+            if len(left_hand_side_tokens) != 1 or left_hand_side_tokens[0] not in self.__nonterminals:
+                return False
+
+        return True
+
---
+++ Labs/Lab-6 (officially Lab-5, but I confused the project names)/main.py
@@ -0,0 +1,13 @@
+from Grammar import Grammar
+
+def main():
+    grammar = Grammar("g1.txt")
+    print("Grammar file content: {}".format(grammar.read_grammar()))
+    print("Non-terminals: {}".format(grammar.get_set_of_nonterminals()))
+    print("Terminals: {}".format(grammar.get_set_of_terminals()))
+    print("Set of productions: {}".format(grammar.get_set_of_productions()))
+    nonterminal = 'B'
+    print("Set of productions for non-terminal {}: {}".format(nonterminal, grammar.get_productions_of_a_nonterminal(nonterminal)))
+    print("Is the grammar context-free? {}".format("Yes" if grammar.check_if_context_free() else "No"))
+
+main()
